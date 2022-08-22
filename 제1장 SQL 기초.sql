ddf8ab1b7c092bf7d520b718269da811393965eb
SBUX <sinsunkangg@naver.com>
2022-08-22 14:02:48 +0900

    2022-08-22 SQL 문제

--- 제1장 SQL 기초.sql
+++ 제1장 SQL 기초.sql
@@ -41,5 +41,41 @@ delete from `User1` where `uid`='A101';
 delete from `User1` where `uid`='A102' and `age`=25;
 delete from `User1` where `age`>=30;
 #실습 1-7
+create table `TblUser`(
+		`userId`	VARCHAR(10),
+        `userName`	VARCHAR(10),
+        `userHp`	CHAR(13),
+        `userAge`	TINYINT,
+        `userAddr`	VARCHAR(20)
+);
+create table `TblProduct`(
+		`prdCode`		INT,
+        `prdName`		VARCHAR(10),
+        `prdPrice`		INT,
+        `prdAmount`		INT,
+        `prdCompany`	VARCHAR(10),
+        `prdMarkeDate`	DATE
+);
 #실습 1-8
+insert into `TblUser` VALUES('p101', '김유신', '010-1234-1001', '25', '서울시 중구');
+insert into `TblUser` VALUES('p102', '김춘추', '010-1234-1002', '23', '부산시 금정구');
+insert into `TblUser` (`userId`, `userName`, `userAge`, `userAddr`) VALUES ('A103', '장보고', 31, '경기도 광주군');
+insert into `TblUser` (`userId`, `userName`, `userAddr`) VALUES ('A103', '장보고', '경남 창원시');
+insert into `TblUser` SET`userId`='A105', `userName`='이순신', `userHp`='010-1234-1005', `userAge`=50;
+
+insert into `TblProduct` values(1, '냉장고', 800, 10, 'LG', 2022-01-06);
+insert into `TblProduct` values(2, '노트북', 1200, 20, '삼성', 2022-01-06);
+insert into `TblProduct` values(3, 'TV', 1400, 6, 'LG', 2022-01-06);
+insert into `TblProduct` values(4, '세탁기', 1000, 8, 'LG', 2022-01-06);
+
+
 #실습 1-9
+
+
+
+
+
+
+
+
+
